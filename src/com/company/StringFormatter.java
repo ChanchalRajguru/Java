92f9dc3db85dad70c9e50ad275781bd950e472a1
ChanchalRajguru <rajguruchanchal@gmail.com>
2020-06-07 23:02:32 +0100

    String Formatter float

--- src/com/company/StringFormatter.java
+++ src/com/company/StringFormatter.java
@@ -11,12 +11,17 @@ public class StringFormatter {
         System.out.printf("\nThe total amount is %-10d.", 5);
 
         for(int i =0; i<14; i++){
-            System.out.printf("\n%2d: Here is some text", i);
+            ;//acts as pass in Python
+//            System.out.printf("\n%2d: Here is some text", i);
         }
         for(int i =0; i<14; i++){
-            System.out.printf("\n%-2d: Another text", i);
+            ;
+//            System.out.printf("\n%-2d: Another text", i);
         }
-        System.out.println("The ");
+        //Formatting floating point value
+        System.out.printf("\nThe float is %.2f.", 314.372877);
+        System.out.printf("\nThe float is %7.2f.", 314.372877);
+        System.out.printf("\nThe float is %-7.2f.", 314.372877);
 
     }
 }
