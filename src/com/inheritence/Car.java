b0f9fe54a372421a2a74963bf2d996784db3cf9a
ChanchalRajguru <rajguruchanchal@gmail.com>
2020-06-09 17:07:13 +0100

    added override method

--- src/com/inheritence/Car.java
+++ src/com/inheritence/Car.java
@@ -1,6 +1,11 @@
 package com.inheritence;
 
 public class Car extends Machine {
+    @Override
+    public void start(){
+        System.out.println("Car Started!");
+    }
+
     public void wipeWindShield(){
         System.out.println("Wiping wind shield.");
     }
